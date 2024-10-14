9a16c1c470acbcb824a5b126ed8f561401169c69
Bart <bart@myotis>
2024-10-14 17:51:35 +0200

    fix checks

--- tests/testthat/test-get_pvol_dk.R
+++ tests/testthat/test-get_pvol_dk.R
@@ -1,5 +1,5 @@
-test_that("Pvol for German can be downloaded", {
-  testthat::skip()
+test_that("Pvol for Danish can be downloaded", {
+  testthat::skip("Because no key for Denmark is available in the testing environment")
   expect_s3_class(pvol<-get_pvol("dkbor", time<-lubridate::floor_date(as.POSIXct(Sys.time(), tz = "Europe/Helsinki")-lubridate::hours(10), "5 mins")-lubridate::days(90), param = "all"),"pvol")
   expect_true(bioRad::is.pvol(pvol))
   expect_identical(lubridate::floor_date(pvol$datetime,'5 mins'), lubridate::with_tz(time,"UTC"))
--- tests/testthat/test-get_pvol_ee.R
+++ tests/testthat/test-get_pvol_ee.R
@@ -2,10 +2,13 @@ test_that("Pvol for estonia can be downloaded", {
   testthat::skip_if_offline()
   withr::local_options(list(httr2_progress = FALSE))
   # The api frequently sends a 429 response therefore test is allowed to fail
-  testthat::show_failure(expect_s3_class(pvol <- suppressMessages(get_pvol("eesur",
+  get_pvol_q<-purrr::quietly(get_pvol)
+  testthat::show_failure(pvol<-expect_no_error(get_pvol_q("eesur",
     time <- as.POSIXct("2024-4-4 20:00:00", tz = "Europe/Helsinki"),
     param = "all"
-  )), "pvol"))
+  )$result))
+  skip_if_not(exists("pvol"))
+  expect_s3_class(pvol,'pvol')
   expect_true(bioRad::is.pvol(pvol))
   expect_identical(pvol$datetime, lubridate::with_tz(time, "UTC"))
 })
