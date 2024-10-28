34054eb0076faf46c473678e717436840b256d89
PietrH <pieterhuy@gmail.com>
2024-10-28 13:57:36 +0100

    Change spacing for style, space between anonymous function init.

--- tests/testthat/test-get_pvol.R
+++ tests/testthat/test-get_pvol.R
@@ -1,4 +1,5 @@
-# Country specific tests are in the respective country specific `get_pvol_` files
+# Country specific tests are in the respective country specific `get_pvol_`
+# files
 
 test_that("get_pvol radar argument", {
   expect_error(get_pvol(), class = "getRad_error_radar_not_character")
@@ -8,6 +9,7 @@ test_that("get_pvol radar argument", {
   expect_error(get_pvol(c("nlhrw", "nldhl", "nlhrw"), time = as.POSIXct(Sys.Date())), class = "getRad_error_radar_not_character")
   expect_error(get_pvol("nnhrw", time = as.POSIXct(Sys.Date())), class = "getRad_error_no_function_for_radar_with_country_code")
 })
+
 test_that("get_pvol time argument", {
   expect_error(get_pvol("nlhrw", time = "asdf"), class = "getRad_error_time_not_correct")
   expect_error(get_pvol("nlhrw"), class = "getRad_error_time_not_correct")
@@ -21,22 +23,32 @@ test_that("multiple radars work", {
   skip_if_offline()
   expect_true(is.list(pvl <- get_pvol(r <- c("fianj", "dehnr"), time = as.POSIXct(Sys.Date()))))
   expect_true(all(unlist(lapply(pvl, bioRad::is.pvol))))
-  expect_identical(lapply(pvl, \(x)x$radar), as.list(r))
+  expect_identical(
+    lapply(pvl, \(x) x$radar),
+    as.list(r)
+  )
 })
-
 
 test_that("multiple timestamps work", {
   skip_if_offline()
   expect_true(is.list(pvl <- get_pvol(c("fianj"), time = t <- (c(-300, -900) + as.POSIXct(Sys.Date())))))
   expect_true(all(unlist(lapply(pvl, bioRad::is.pvol))))
-  expect_identical(lapply(pvl, \(x)x$datetime), as.list(t))
+  expect_identical(
+    lapply(pvl, \(x) x$datetime),
+    as.list(t)
+  )
 })
-
 
 test_that("multiple timestamps and radars work", {
   skip_if_offline()
   expect_true(is.list(pvl <- get_pvol(r <- c("fivim", "deess"), time = t <- (c(-300, -900) + as.POSIXct(Sys.Date())))))
   expect_true(all(unlist(lapply(pvl, bioRad::is.pvol))))
-  expect_identical(lapply(pvl, \(x)x$datetime), as.list(rep(t, each = 2)))
-  expect_identical(lapply(pvl, \(x)x$radar), as.list(rep(r, 2)))
+  expect_identical(
+    lapply(pvl, \(x) x$datetime),
+    as.list(rep(t, each = 2))
+  )
+  expect_identical(
+    lapply(pvl, \(x) x$radar),
+    as.list(rep(r, 2))
+  )
 })
