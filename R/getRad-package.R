7b3ac7635b7f4f1d984ecef0704b95517c6318e3
PietrH <pieterhuy@gmail.com>
2024-11-07 15:33:43 +0100

    Update getRad-package.R

--- R/getRad-package.R
+++ R/getRad-package.R
@@ -9,12 +9,10 @@
 #' @importFrom httr2 req_url_path_append
 #' @importFrom httr2 req_url_query
 #' @importFrom httr2 req_user_agent
-#' @importFrom httr2 request
 #' @importFrom httr2 resp_body_html
 #' @importFrom httr2 resp_body_json
 #' @importFrom httr2 req_body_json
 #' @importFrom httr2 req_retry
-#' @importFrom httr2 resp_status
 #' @importFrom cli cli_abort cli_inform
 #' @importFrom rlang is_scalar_character is_character
 ## usethis namespace: end
