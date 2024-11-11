e20338abacf3d6fb3e40b818996eee3b72cb8242
Bart <bart@myotis>
2024-11-11 13:20:15 +0100

    Seems ok to enable test

--- tests/testthat/test-get_pvol_ee.R
+++ tests/testthat/test-get_pvol_ee.R
@@ -2,11 +2,10 @@ test_that("Pvol for estonia can be downloaded", {
 skip_if_offline()
   withr::local_options(list(httr2_progress = FALSE))
   # The api frequently sends a 429 response therefore test is allowed to fail
-  get_pvol_q <- purrr::quietly(get_pvol)
-  show_failure(pvol <- expect_no_error(get_pvol_q("eesur",
-    time <- as.POSIXct("2024-4-4 20:00:00", tz = "Europe/Helsinki"),
+  pvol <- expect_no_error(get_pvol("eesur",
+    time <- as.POSIXct("2024-4-4 21:00:00", tz = "Europe/Helsinki"),
     param = "all"
-  )$result))
+  ))
   skip_if_not(exists("pvol"))
   expect_s3_class(pvol, "pvol")
   expect_true(bioRad::is.pvol(pvol))
