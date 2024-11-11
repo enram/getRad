e1f1852a78e0c828b5e8def8ebc8cf387e03f6c7
Bart <bart@myotis>
2024-11-11 10:29:56 +0100

    fix tests

--- tests/testthat/test-get_pvol_nl.R
+++ tests/testthat/test-get_pvol_nl.R
@@ -1,6 +1,6 @@
 test_that("Pvol for the Netherlands can be downloaded", {
   skip_if_offline()
-  kip_if(Sys.which("KNMI_vol_h5_to_ODIM_h5") == "")
+  skip_if(Sys.which("KNMI_vol_h5_to_ODIM_h5") == "")
   withr::local_options(list("keyring_backend" = "env"))
   # get public key here https://developer.dataplatform.knmi.nl/open-data-api#token
   withr::local_envvar(
@@ -21,7 +21,7 @@ test_that("Pvol for the Netherlands can be downloaded", {
 
 test_that("failure to find converter", {
   skip_if_offline()
-  kip_if_not(Sys.which("KNMI_vol_h5_to_ODIM_h5") == "")
+  skip_if_not(Sys.which("KNMI_vol_h5_to_ODIM_h5") == "")
 
   withr::local_options(list("keyring_backend" = "env"))
   # get public key here https://developer.dataplatform.knmi.nl/open-data-api#token
