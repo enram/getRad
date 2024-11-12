08f8a2c2ed4c59d0d35e78361d2b8eab8fcaf23b
PietrH <pieterhuy@gmail.com>
2024-11-12 15:27:06 +0100

    Add base replacement for stringr::str_replace()

--- R/utils.R
+++ R/utils.R
@@ -10,8 +10,25 @@
 #'
 #' @examples
 #' string_extract("Hello World", "o W")
-#' # Output: "o W"
 #'
 string_extract <- function(string, pattern) {
   regmatches(string, regexpr(pattern, text = string, perl = TRUE))
+}
+
+#' Replace a pattern in a string with a replacement.
+#'
+#' This function uses regular expressions to replace a pattern in a string with a specified replacement.
+#' This is a base replacement of stringr::str_replace()
+#'
+#' @param string The input string.
+#' @param pattern The pattern to search for in the string.
+#' @param replacement The replacement string.
+#'
+#' @return The modified string with the pattern replaced.
+#'
+#' @examples
+#' string_replace("I'm looking for radars", "radar", "bird")
+#'
+string_replace <- function(string, pattern, replacement) {
+  sub(pattern, replacement, string, perl = TRUE)
 }
