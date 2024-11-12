5106db7926bd8fdeee8ca8fab3ce8eae4788a91c
PietrH <pieterhuy@gmail.com>
2024-11-12 15:02:25 +0100

    Add basic tests for `get_vpts()` functionality

---
+++ tests/testthat/test-get_vpts.R
@@ -0,0 +1,52 @@
+test_that("get_vpts() can fetch vpts data for a single radar and time", {
+  expect_s3_class(
+    # A known radar and date combination that ALOFT has data for
+    get_vpts(radar = "bejab", date = "2023-01-01"),
+    "data.frame"
+  )
+})
+
+test_that("get_vpts() can fetch vpts data for multiple radars", {
+  expect_s3_class(
+    get_vpts(radar = c("bejab", "bewid"), date = "2023-01-01"),
+    "data.frame"
+  )
+})
+
+test_that("get_vpts() can fetch vpts data for a date range", {
+  expect_s3_class(
+    get_vpts(radar = "bejab",
+             lubridate::interval(
+               lubridate::ymd("2023-01-01"),
+               lubridate::ymd("2023-01-02")
+             )
+    ),
+    "data.frame"
+  )
+})
+
+test_that("get_vpts() returns an error for a bad radar", {
+  # Radar not found in ALOFT coverage
+  expect_error(
+    get_vpts(radar = "notaradar", date = "2023-01-01"),
+    class = "getRad_error_radar_not_found"
+  )
+  # Radar is not a character vector
+  expect_error(
+    get_vpts(radar = 1:3, date = "2023-01-01"),
+    class = "getRad_error_radar_not_single_string"
+  )
+})
+
+test_that("get_vpts() returns an error for a bad time argument", {
+  # Date not found in ALOFT coverage
+  expect_error(
+    get_vpts(radar = "bejab", date = "2023-01-02"),
+    class = "getRad_error_date_not_found"
+  )
+  # Time is not parsable to a date or interval
+  expect_error(
+    get_vpts(radar = "bejab", date = 1:3),
+    class = "getRad_error_date_parsable"
+  )
+})
