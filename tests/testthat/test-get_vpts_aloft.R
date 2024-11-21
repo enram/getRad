eb167e0360125c6faa55e87d02a8a417cbff63a9
PietrH <pieterhuy@gmail.com>
2024-11-21 14:06:15 +0100

    Add test for default coverage behavior + basic functionality

--- tests/testthat/test-get_vpts_aloft.R
+++ tests/testthat/test-get_vpts_aloft.R
@@ -34,3 +34,57 @@ test_that("get_vpts_aloft() returns error when multiple radars are queried", {
     class = "getRad_error_radar_not_single_string"
   )
 })
+
+test_that("get_vpts_aloft() can fetch vtps data from aloft", {
+  skip_if_offline()
+
+  aloft_vpts_tbl <- get_vpts_aloft(
+    radar = c("depro"),
+    rounded_interval = lubridate::interval("2024-08-12", "2024-08-13"),
+    source = "baltrad"
+  )
+  # Test that a tibble is returned
+  expect_type(
+    aloft_vpts_tbl,
+    "list"
+  )
+
+  expect_s3_class(
+    aloft_vpts_tbl,
+    "tbl_df"
+  )
+
+  # Test that the tibble has the expected columns
+  expect_named(
+    aloft_vpts_tbl,
+    c(
+      "source",
+      "radar",
+      "datetime",
+      "height",
+      "u",
+      "v",
+      "w",
+      "ff",
+      "dd",
+      "sd_vvp",
+      "gap",
+      "eta",
+      "dens",
+      "dbz",
+      "dbz_all",
+      "n",
+      "n_dbz",
+      "n_all",
+      "n_dbz_all",
+      "rcs",
+      "sd_vvp_threshold",
+      "vcp",
+      "radar_latitude",
+      "radar_longitude",
+      "radar_height",
+      "radar_wavelength",
+      "source_file"
+    )
+  )
+})
