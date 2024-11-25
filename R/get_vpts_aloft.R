37f61608f68ed2ab71ddf9245ca00c1006c440b4
PietrH <pieterhuy@gmail.com>
2024-11-25 16:08:49 +0100

    Abstract fetching form web away to helper, to make function easier to read

--- R/get_vpts_aloft.R
+++ R/get_vpts_aloft.R
@@ -96,55 +96,7 @@ get_vpts_aloft <- function(radar_odim_code,
   ## criteria
 
   paste(aloft_data_url, s3_paths, sep = "/") |>
-    purrr::map(httr2::request) |>
-    # Identify ourselves in the request
-    purrr::map(req_user_agent_getrad) |>
-    # Set retry conditions
-    purrr::map(~ httr2::req_retry(
-      .x,
-      max_tries = 15, backoff = \(x) sqrt(x) * 2,
-      is_transient = \(resp) resp_status(resp) %in% c(429),
-      retry_on_failure = TRUE
-    )) |>
-    # Perform the requests in parallel
-    httr2::req_perform_parallel() |>
-    # Fetch the response bodies and parse it using vroom
-    ## A helper in bioRad (validate_vpts()) that we call indirectly via
-    #" bioRad::as.vpts() currently doesn't support factors: bioRad v0.8.1
-    purrr::map(httr2::resp_body_string) |>
-    purrr::map(~ vroom::vroom(delim = ",",
-                              I(.x),
-                              col_types =
-                                list(
-                                  radar = vroom::col_factor(),
-                                  datetime = vroom::col_datetime(),
-                                  height = vroom::col_integer(),
-                                  u = vroom::col_double(),
-                                  v = vroom::col_double(),
-                                  w = vroom::col_double(),
-                                  ff = vroom::col_double(),
-                                  dd = vroom::col_double(),
-                                  sd_vvp = vroom::col_double(),
-                                  gap = vroom::col_logical(),
-                                  eta = vroom::col_double(),
-                                  dens = vroom::col_double(),
-                                  dbz = vroom::col_double(),
-                                  dbz_all = vroom::col_double(),
-                                  n = vroom::col_integer(),
-                                  n_dbz = vroom::col_integer(),
-                                  n_all = vroom::col_integer(),
-                                  n_dbz_all = vroom::col_integer(),
-                                  rcs = vroom::col_double(),
-                                  sd_vvp_threshold = vroom::col_double(),
-                                  vcp = vroom::col_integer(),
-                                  radar_longitude = vroom::col_double(),
-                                  radar_latitude = vroom::col_double(),
-                                  radar_height = vroom::col_integer(),
-                                  radar_wavelength = vroom::col_double(),
-                                  source_file = vroom::col_character()
-                                ),
-                              show_col_types = NULL,
-                              progress = FALSE)) |>
+    read_vpts_from_url() |>
     # Add a column with the radar source to not lose this information
     purrr::map2(s3_paths, ~dplyr::mutate(.x,
                                          source = string_extract(.y,
