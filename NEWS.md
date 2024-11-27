477f7aa109ef6fb0e38fe73882d1d7301235df23
PietrH <pieterhuy@gmail.com>
2024-11-27 14:44:14 +0100

    Make `aloft_data_coverage()` public

--- NEWS.md
+++ NEWS.md
@@ -2,4 +2,5 @@
 
 * Initial package development
 * Add `get_pvol` function to download polar volumes for 6 countries
-* Add `get_vpts` function to download volume scans from the [Aloft data repository](https://aloftdata.eu/)
+* Add `get_vpts` function to download vertical profile time series from the [Aloft data repository](https://aloftdata.eu/)
+* Add `aloft_data_coverage` function to fetch an overview table of the files available on the [Aloft data repository](https://aloftdata.eu/)
---
+++ R/aloft_data_coverage.R
@@ -0,0 +1,26 @@
+#' Fetch the coverage file from the aloft data repository
+#'
+#' The coverage file provides an overview of what files are available on the
+#' aloft data repository. Specifically it lists the directories that are
+#' available as well as the number of files in every directory.
+#'
+#' @return A data.frame of the coverage file on the aloft data repository
+#' @export
+#'
+#' @examplesIf interactive()
+#' aloft_data_coverage()
+aloft_data_coverage <- function() {
+  # Discover what data is available for the requested radar and time interval
+  coverage_url <- "https://aloftdata.s3-eu-west-1.amazonaws.com/coverage.csv"
+  coverage <-
+    vroom::vroom(coverage_url, progress = FALSE, show_col_types = FALSE) |>
+    dplyr::mutate(source = string_extract(.data$directory, ".+(?=\\/hdf5)"),
+                  radar = string_extract(.data$directory, "(?<=hdf5\\/)[a-z]{5}"),
+                  date = as.Date(
+                    string_extract(.data$directory,
+                                   "[0-9]{4}\\/[0-9]{2}\\/[0-9]{2}$")
+                  )
+    )
+
+  return(coverage)
+}
